c9f81021e39a673405a533ea92db1ff14e397bb0
Bogdan Kologrivov <bogdans.kologrivovs@gmail.com>
2025-04-29 23:47:40 +0300

    Add files via upload

---
+++ app/json_writer.py
@@ -0,0 +1,50 @@
+"""Prepares and writes data to JSON"""
+
+from app.file_writer import FileWriter
+
+from typing import Dict, Any
+import json
+
+class JsonWriter(FileWriter):
+    # prepares data in the dictionary form
+    def get_data(self) -> Dict[str, Any]:
+        data = {}
+
+        data['count'] = {}
+        data['count']['total_count'] = self.dragonfly.total_count
+        data['count']['count_by_year'] = self.dragonfly.count_by_year
+        data['count']['count_by_square'] = self.dragonfly.count_by_square
+
+        data['temperature'] = {}
+        data['temperature']['avg_temp_by_year'] = self.dragonfly.avg_temp_by_year
+        data['temperature']['square_year_temp'] = self.dragonfly.square_year_temp
+        data['temperature']['avg_temp_by_square'] = self.dragonfly.avg_temp_by_square
+
+        data['wind'] = {}
+        data['wind']['avg_wind_by_year'] = self.dragonfly.avg_wind_by_year
+        data['wind']['square_year_wind'] = self.dragonfly.square_year_wind
+        data['wind']['avg_wind_by_square'] = self.dragonfly.avg_wind_by_square
+
+        data['cloudiness'] = {}
+        data['cloudiness']['avg_cloudiness_by_year'] = self.dragonfly.avg_clouds_by_year
+        data['cloudiness']['square_year_cloudiness'] = self.dragonfly.square_year_clouds
+        data['cloudiness']['avg_cloudiness_by_square'] = self.dragonfly.avg_clouds_by_square
+
+        data['water'] = {}
+        data['water']['year_water_types'] = self.dragonfly.year_water_types
+        data['water']['square_year_water'] = self.dragonfly.square_year_water
+
+        data['shading'] = {}
+        data['shading']['year_shading_types'] = self.dragonfly.year_shading_types
+        data['shading']['square_year_shading'] = self.dragonfly.square_year_shading
+
+        return data
+    
+    # saves given dictionary to JSON file
+    @staticmethod
+    def save(data: Dict[str, Any], output_filename: str, min: bool = False):
+        with open(output_filename, 'w', encoding = 'utf-8') as file:
+            if min:
+                json.dump(data, file, separators = (',', ':'))
+            else:
+                json.dump(data, file, indent = 4, ensure_ascii = False)
